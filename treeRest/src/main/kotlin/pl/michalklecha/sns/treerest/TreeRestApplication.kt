a0d2b292be569efa8ce46c4584dcb37b2aa1d7c9
michalklecha <michalklecha.0@gmail.com>
2018-05-11 15:05:00 +0200

    kotlin rest added

---
+++ treeRest/src/main/kotlin/pl/michalklecha/sns/treerest/TreeRestApplication.kt
@@ -0,0 +1,11 @@
+package pl.michalklecha.sns.treerest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TreeRestApplication
+
+fun main(args: Array<String>) {
+    runApplication<TreeRestApplication>(*args)
+}
